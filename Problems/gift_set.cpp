71d2a887de02702ab3ab5fdfc0c83b4f8fb1efcb
iamkabilan <kabilan185@gmail.com>
2021-07-16 18:33:35 +0530

    changed

---
+++ Problems/gift_set.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+
+	for(int i=0;i<t;i++) {
+		int x,y,a,b,count=0;
+		cin>>x>>y>>a>>b;
+
+		while((x>a and x>b) and (y>a and y>b)) {
+			count=count+1;
+			if(a>x)
+		}
+	}
+}
