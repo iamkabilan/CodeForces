6a19557a7492e0eb94a39c3e6fef62bb5d9b20b9
kabilan <kabilan185@gmail.com>
2021-08-04 12:25:25 +0530

    New files

---
+++ Problems/permutation.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--) {
+		int n;
+		cin>>n;
+		n=2*n;
+		int arr[n];
+		vector<int>v;
+
+		for(int i=0;i<n;i++) {
+			cin>>arr[i];
+		}
+
+		for(int i=0;i<n;i++) {
+			int temp=arr[i];
+			int a=0;
+			bool t=false;
+			for(int j=0;j<v.size();j++) {
+				if(v[j]==temp) {
+					t=true;
+				}
+			}
+			if(t==false) {
+				v.push_back(temp);
+			}
+		}
+		for(auto i:v) {
+			cout<<i<<" ";
+		}
+		cout<<endl;
+	}
+    return 0;
+}
